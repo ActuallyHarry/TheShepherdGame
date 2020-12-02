d892921657a7453bd446e1ada4652acd7fdaaa8d
TheOrangePi <TheOrangePi@users.noreply.github.com>
2020-12-02 15:51:20 +1100

    Revert "BasePrototype"
    This reverts commit 384ea6d9921a88bb6a3b9513f5135e0ff9d20205.

--- TheShepherdGame/Assets/Scripts/Actors/Detection/DetectionBevaiour.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class DetectionBehaviour: ScriptableObject
-{
-    public abstract List<Transform> GetContext(Actor actor, float inRadius);
-}
